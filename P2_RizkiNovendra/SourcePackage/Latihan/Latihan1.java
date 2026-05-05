d85606da33f728260bb8ed022e7e1efd6daf9bb8
Rizki Novendra <rizkinovendra9@gmail.com>
2026-05-05 18:03:54 +0700

    Update tugas

--- P2_RizkiNovendra/SourcePackage/Latihan/Latihan1.java
+++ P2_RizkiNovendra/SourcePackage/Latihan/Latihan1.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Latihan1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String nama; int usia;
+        String nama;@SuppressWarnings("unused")
+ int usia;
         System.out.print("Input Nama: ");
         nama=input.nextLine();
         System.out.print("Nama: " + nama);
